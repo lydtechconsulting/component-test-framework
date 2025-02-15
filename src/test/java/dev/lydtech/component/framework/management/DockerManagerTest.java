f5181ec71ac94d27bc50f8db70269aa70539bb6d
rgolder1 <rgolder1@yahoo.com>
2025-02-15 18:11:10 +0000

    feat: Supports loading configuration overrides from a properties or YAML file.

--- src/test/java/dev/lydtech/component/framework/management/DockerManagerTest.java
+++ src/test/java/dev/lydtech/component/framework/management/DockerManagerTest.java
@@ -1,10 +1,12 @@
 package dev.lydtech.component.framework.management;
 
 import java.util.Collections;
+import java.util.Properties;
 
 import com.github.dockerjava.api.DockerClient;
 import com.github.dockerjava.api.command.ListContainersCmd;
 import com.github.dockerjava.api.model.Container;
+import dev.lydtech.component.framework.configuration.TestcontainersConfiguration;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -24,6 +26,8 @@ public class DockerManagerTest {
 
     @BeforeEach
     public void setUp() {
+        // Configure with the default properties.
+        TestcontainersConfiguration.configure(new Properties());
         mocks = MockitoAnnotations.openMocks(this);
         dockerClient = Mockito.mock(DockerClient.class);
     }
@@ -41,6 +45,10 @@ public class DockerManagerTest {
         MatcherAssert.assertThat(dockerClient, is(notNullValue()));
     }
 
+    /**
+     * If the main container is already running (the service under test), and the Testcontainers container is not present
+     * (which if it was would be responsible for stopping the containers in the test), then do not perform setup.
+     */
     @Test
     public void testShouldPerformSetup_WhenMainContainerIsRunning() {
         Container mainContainer = mock(Container.class);
@@ -56,10 +64,31 @@ public class DockerManagerTest {
         MatcherAssert.assertThat(result, is(false));
     }
 
+    /**
+     * If the main container is not running (the service under test), then perform setup, even if the Testcontainers
+     * container is running.
+     */
     @Test
     public void testShouldPerformSetup_WhenTestcontainersIsRunning() {
         Container testContainer = mock(Container.class);
         when(testContainer.getNames()).thenReturn(new String[]{"/testcontainers-ryuk"});
+
+        ListContainersCmd listContainersCmdMock = mock(ListContainersCmd.class);
+        when(dockerClient.listContainersCmd()).thenReturn(listContainersCmdMock);
+        when(listContainersCmdMock.exec()).thenReturn(Collections.singletonList(testContainer));
+
+        boolean result = DockerManager.shouldPerformSetup(dockerClient);
+
+        MatcherAssert.assertThat(result, is(true));
+    }
+
+    /**
+     * If no containers are running, then perform setup.
+     */
+    @Test
+    public void testShouldPerformSetup_WhenNoContainerIsRunning() {
+        Container testContainer = mock(Container.class);
+        when(testContainer.getNames()).thenReturn(new String[]{""});
 
         ListContainersCmd listContainersCmdMock = mock(ListContainersCmd.class);
         when(dockerClient.listContainersCmd()).thenReturn(listContainersCmdMock);
